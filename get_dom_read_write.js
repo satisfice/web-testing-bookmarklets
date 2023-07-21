3fd4b6d5db7249ff4f3c5f118a9a66c40eb8b543
James Bach <james@satisfice.com>
2023-07-21 01:04:03 -0700

    added .json to filename

--- get_dom_read_write.js
+++ get_dom_read_write.js
@@ -6,7 +6,7 @@ javascript:
 		{
   			var hostname = window.location.hostname.substring(0, window.location.hostname.lastIndexOf('.')).replaceAll(".", "_");
   			var pathname = window.location.pathname ? window.location.pathname.replaceAll('/', '_'): "_";
-	    		var filename = hostname + pathname + "_DOM_read_writes";
+	    		var filename = hostname + pathname + "_DOM_read_writes.json";
 			return filename;
 		}
 		function download(text, name, type) 
