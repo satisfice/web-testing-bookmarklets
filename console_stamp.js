85b17038a9e0516fb2e352fadd56204c9a330fb3
James Bach <james@satisfice.com>
2023-09-02 21:55:24 -0700

    New! logs info for testing.

---
+++ console_stamp.js
@@ -0,0 +1,11 @@
+javascript: 
+(
+	()=>
+	{
+		the_title = document.title;
+		the_place = window.location.href;
+		the_text = window.getSelection().toString();
+		the_time = Date().valueOf();
+		console.log("*** Test Stamp ***\n" + "TIME: " + the_time + "\n" + "TITLE: " + the_title + "\n" + "URL: " + the_place + "\n" + "SELECTED TEXT: " + the_text + "\n" );		
+	}
+)();
