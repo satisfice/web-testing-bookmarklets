bada503bc45acbb081b1f5cc552dc71c68eba6d1
James Bach <james@satisfice.com>
2023-09-06 20:04:09 -0700

    added prompt

--- console_stamp.js
+++ console_stamp.js
@@ -6,6 +6,11 @@ javascript:
 		the_place = window.location.href;
 		the_text = window.getSelection().toString();
 		the_time = Date().valueOf();
-		console.log("*** Test Stamp ***\n" + "TIME: " + the_time + "\n" + "TITLE: " + the_title + "\n" + "URL: " + the_place + "\n" + "SELECTED TEXT: " + the_text + "\n" );		
+		the_message = prompt("What news?");
+		console.log("*** Test Stamp ***" + "\nTIME: " + the_time + "\nMessage: " + the_message + "\nTITLE: " + the_title + "\nURL: " + the_place + "\n" );	
+		if (the_text == "")
+		{
+			console.log("SELECTED TEXT: " + the_text + "\n"); 
+		}
 	}
 )();
