6f1ab6e2aa4beacf18e48c969ca4f9446eec477d
James Bach <james@satisfice.com>
2023-11-11 22:04:13 -0800

    grab the DOM as HTML

---
+++ get_dom_raw.js
@@ -0,0 +1,29 @@
+javascript: 
+(
+	()=>
+	{
+		function downloadName()
+		{
+  			var hostname = window.location.hostname.substring(0, window.location.hostname.lastIndexOf('.')).replaceAll(".", "_");
+  			var pathname = window.location.pathname ? window.location.pathname.replaceAll('/', '_'): "_";
+	    		var filename = hostname + pathname + "_DOM_raw.json";
+			return filename;
+		}
+		function download(text, name, type) 
+		{  
+			var a = document.createElement("a"); 
+			var file = new Blob([text], {type: type});  
+			a.href = URL.createObjectURL(file);  
+			a.download = name;  
+			a.click()
+		}
+		dict = document.documentElement.outerHTML;
+		the_title = document.title;
+		the_place = window.location.href;
+		the_text = window.getSelection().toString();
+		the_time = Date().valueOf();
+		console.log("*** Test Stamp ***\n(Downloaded raw DOM)\n" + "TIME: " + the_time + "\n" + "TITLE: " + the_title + "\n" + "URL: " + the_place + "\n" + "SELECTED TEXT: " + the_text + "\n" );		
+
+		download(dict,downloadName(),"application/json");
+	}
+)();
